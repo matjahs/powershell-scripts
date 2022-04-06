54a951cc63094c0aaeaa81504d90d7f59db69c8d
Emile Cox <Emile.Cox@radboudumc.nl>
2022-04-06 13:45:55 +0000

    Updated run-packer.ps1

--- Packer/run-packer.ps1
+++ Packer/run-packer.ps1
@@ -2,9 +2,6 @@
 
 param(
     [parameter(Mandatory = $true)]
-    [string]$vsphere_server,
-
-    [parameter(Mandatory = $true)]
     [string]$vsphere_user,
 
     [parameter(Mandatory = $true)]
@@ -24,4 +21,16 @@ param(
     [string]$disksize
 )
 
-.\packer.exe build -var 'vsphere-server=$vsphere_server' -var 'vsphere-user=$vsphere_user' -var 'vsphere-password=$vsphere_password' -var 'vm-name=$vmname' -var 'vsphere-cluster=$cluster' -var 'os-disk-size=$disksize' 'Win2019\windows2019.json'
+# Define target environment
+switch ( $targetenvironment ) {
+    "DEV" {
+        $Vcenters = @("umcvct01.umcn.nl")
+        Write-Log -Message "The following vCenter servers have been filtered {0}" -Arguments ( $Vcenters -join ", " )  -Level DEBUG
+    }
+    "PRD" {
+        $Vcenters = @("umcvcp01.umcn.nl")
+        Write-Log -Message "The following vCenter servers have been filtered {0}" -Arguments ( $Vcenters -join ", " )  -Level DEBUG
+    }
+}
+
+.\packer.exe build -var 'vsphere-server=$Vcenters' -var 'vsphere-user=$vsphere_user' -var 'vsphere-password=$vsphere_password' -var 'vm-name=$vmname' -var 'vsphere-cluster=$cluster' -var 'os-disk-size=$disksize' 'Win2019\windows2019.json'
