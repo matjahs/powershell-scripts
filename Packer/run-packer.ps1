34cbc54ab1bd4df097cf2d052c0e1169a90d35b7
Emile Cox <Emile.Cox@radboudumc.nl>
2022-04-06 13:23:08 +0000

    Updated run-packer.ps1

--- Packer/run-packer.ps1
+++ Packer/run-packer.ps1
@@ -1,1 +1,27 @@
-.\packer.exe build -var 'vsphere-server=test' -var 'vsphere-user=test' -var 'vsphere-password=test' -var 'vm-name=test' -var 'vsphere-cluster=test' -var 'os-disk-size=50' 'Win2019\windows2019.json'
+[CmdletBinding()]
+
+param(
+    [parameter(Mandatory = $true)]
+    [string]$vsphere_server,
+
+    [parameter(Mandatory = $true)]
+    [string]$vsphere_user,
+
+    [parameter(Mandatory = $true)]
+    [string]$vsphere_password,
+
+    [parameter(Mandatory = $true)]
+    [ValidateSet('DEV', 'PRD')]
+    [string]$targetenvironment = 'DEV',
+
+    [parameter(Mandatory = $true)]
+    [string]$vmname,
+
+    [parameter(Mandatory = $true)]
+    [string]$cluster,
+
+    [parameter(Mandatory = $true)]
+    [string]$disksize
+)
+
+.\packer.exe build -var 'vsphere-server=$vsphere_server' -var 'vsphere-user=$vsphere_user' -var 'vsphere-password=$vsphere_password' -var 'vm-name=$vmname' -var 'vsphere-cluster=$cluster' -var 'os-disk-size=$disksize' 'Win2019\windows2019.json'
