92533b106cb3e6a520bf3ef83d2d05e0176ab975
Emile Cox <Emile.Cox@radboudumc.nl>
2022-04-12 13:13:08 +0000

    Updated CreateSnapshot.ps1

--- AdjustVM/CreateSnapshot.ps1
+++ AdjustVM/CreateSnapshot.ps1
@@ -98,6 +98,7 @@ try {
 
     Write-Log -Message "Checking if snapshot for {0} must be scheduled" -Arguments $Vm -Level INFO    
     if ($Snapshottime -ne $null) {
+        $Snapshottime = [DateTime]$snapshotTime
         $SnapshotName = 'automated scheduled snapshot for {0}' -f $ChangeNR
         $PoweronName = 'Power on {0} for {1}' -f ($Vm,$ChangeNR)
         $SnapshotDescription = 'automated scheduled snapshot of {0} for {1} created on {2} UTC' -f ($Vm, $requestor, $SnapshotTime)
